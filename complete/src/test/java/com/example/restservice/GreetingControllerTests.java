bd444cde99b02c68d65adb04fa7eaac55b3630bd
alexandrelopes01 <163442457+alexandrelopes01@users.noreply.github.com>
2024-03-28 16:05:08 -0300

    Update GreetingControllerTests.java
    feature/aula-01

--- complete/src/test/java/com/example/restservice/GreetingControllerTests.java
+++ complete/src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -34,12 +34,19 @@ public class GreetingControllerTests {
 	@Autowired
 	private MockMvc mockMvc;
 
+	/*é um framework de teste que permite simular as solicitações HTTP e verificar as respostas recebidas do controlador. */
+
+	/*Eu Alexandre como testador do software gs-rest-service desejo que a classe GreetingControllerTeststenha todos os seus métodos comentados com a descrição do objetivo do teste unitário do referido método. */
+
 	@Test
 	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
 		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
 				.andExpect(jsonPath("$.content").value("Hello, World!"));
 	}
+
+	/*Este é um método de teste que verifica o comportamento do controlador quando nenhum parâmetro é passado na solicitação. Ele usa o método perform do objeto MockMvc para enviar uma solicitação GET para "/greeting" e então verifica se a resposta tem o status HTTP 200 (OK) e se o conteúdo da resposta contém a mensagem "Hello, World!". */
+ 
 
 	@Test
 	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
@@ -49,4 +56,7 @@ public class GreetingControllerTests {
 				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
 	}
 
+	/*Este é outro método de teste que verifica o comportamento do controlador quando um parâmetro "name" é passado na solicitação. Ele também usa o método perform do objeto MockMvc para enviar uma solicitação GET para "/greeting" com o parâmetro "name" definido como "Spring Community". Em seguida, ele verifica se a resposta tem o status HTTP 200 (OK) e se o conteúdo da resposta contém a mensagem "Hello, Spring Community!". */
+
+	/* Esses testes são úteis para garantir que o controlador esteja se comportando conforme o esperado, retornando as mensagens corretas com base nos parâmetros passados na solicitação. Isso ajuda a garantir a qualidade e o funcionamento adequado do aplicativo Spring Boot.*/
 }
